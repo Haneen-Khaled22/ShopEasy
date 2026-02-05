9a2ffdcf3e60bbacb05ed7920e84e579f7052c07
Haneen Khaled <haneenk385@gmail.com>
2026-02-05 02:56:16 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { createBrowserRouter, NavLink, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
 import Contact from './components/Contact/Contact'
